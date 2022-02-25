b2b5e16d751564766827c22b8ecaa0fd0aaf4f93
gfrn <guilherme.freitas@cnpem.br>
2022-02-25 10:18:18 -0300

    Adjusts scheduling, adds queue handler

--- interfaceFF.c
+++ interfaceFF.c
@@ -1,3 +1,5 @@
+#define _GNU_SOURCE
+
 #include <fcntl.h>
 #include <pthread.h>
 #include <stdint.h>
@@ -6,6 +8,9 @@
 #include <sys/types.h>
 #include <termios.h>
 #include <unistd.h>
+#include <poll.h>
+#include <mqueue.h>
+#include <sched.h>
 
 #define PRU1_ADDR 0x4b200000
 #define PRU2_ADDR 0x4b280000
@@ -20,12 +25,52 @@ uint8_t reverseBits8(uint8_t num);
 void adjustVector(uint8_t *data_vector, float current1, float current2,
                   float current3, float current4);
 
-int main(void) {
+int fd;
+pthread_mutex_t serial_mutex;
 
-  int fd;
+void *listenForCommands()
+{
+  struct mq_attr attr;
 
+  attr.mq_maxmsg = 1000;
+  attr.mq_msgsize = 32;
+  attr.mq_flags = 0;
+
+  mqd_t cmd_mq = mq_open("/cmd_mq", (O_RDWR | O_CREAT), 0666, &attr);
+  mqd_t reply_ioc = mq_open("/reply_ioc", (O_RDWR | O_CREAT), 0666, &attr);
+  size_t write_size, reply_size;
+  char cmd_buf[32];
+  char reply_buf[32];
+
+  unsigned int priority;
+
+  struct pollfd pfds[1];
+  pfds[0].fd = fd;
+  pfds[0].events = POLLIN;
+
+  for (;;)
+  {
+    write_size = mq_receive(cmd_mq, cmd_buf, sizeof(cmd_buf), &priority);
+    pthread_mutex_lock(&serial_mutex);
+    write(fd, cmd_buf, write_size);
+
+    if (poll(pfds, 1, 1) < 1)
+    {
+      mq_send(reply_ioc, "\xee", 1, priority);
+    } else {
+      reply_size = read(fd, &reply_buf, sizeof(reply_buf));
+      mq_send(reply_ioc, reply_buf, reply_size, priority);
+    }
+
+    pthread_mutex_unlock(&serial_mutex);
+  }
+}
+
+int main(void)
+{
   fd = open("/dev/ttyUSB0", O_RDWR | O_NOCTTY | O_NDELAY);
-  if (fd == -1) {
+  if (fd == -1)
+  {
     perror("No valid serial device connected");
     return -1;
   }
@@ -71,7 +116,8 @@ int main(void) {
                     PRU2_ADDR + SHRAM_OFFSET);
 
   // SIMULATED VECTOR
-  for (i = 0; i < BUFFER_SIZE; i++) {
+  for (i = 0; i < BUFFER_SIZE; i++)
+  {
     current_up[i] = i * 0.0001;
   }
 
@@ -88,17 +134,32 @@ int main(void) {
                 (reverseBits8((uint8_t)prudata2[11] & 0xFF) << 29);
   oldpos = position[2];
 
-  FILE *f;
-  f = fopen("bench.csv", "w");
+  pthread_t thisThread = pthread_self();
+  pthread_t cmdThread;
 
-  pthread_t this_thread = pthread_self();
+  pthread_create(&cmdThread, NULL, listenForCommands, NULL);
+  pthread_mutex_init(&serial_mutex, NULL);
+
+  cpu_set_t *mainCpuSet;
+  cpu_set_t *cmdCpuSet;
+
+  size_t cpuSetSize;
+
+  CPU_ZERO_S(cpuSetSize, &mainCpuSet);
+  CPU_ZERO_S(cpuSetSize, &cmdCpuSet);
+
+  CPU_SET_S(1, cpuSetSize, mainCpuSet);
+  CPU_SET_S(0, cpuSetSize, cmdCpuSet);
 
   struct sched_param params;
-  params.sched_priority = sched_get_priority_max(SCHED_RR);
-  pthread_setschedparam(this_thread, SCHED_RR, &params);
+  params.sched_priority = sched_get_priority_max(SCHED_FIFO)/2;
+  pthread_setschedparam(thisThread, SCHED_FIFO, &params);
+  pthread_setaffinity_np(thisThread, sizeof(cpu_set_t), mainCpuSet);
+  pthread_setaffinity_np(cmdThread, sizeof(cpu_set_t), cmdCpuSet);
+
   gettimeofday(&tv1, NULL);
-  //for (int i = 0; i < ITERACTIONS; i++)
-  while(1)
+  // for (int i = 0; i < ITERACTIONS; i++)
+  while (1)
   {
     position[0] = reverseBits((uint32_t)prudata1[2]) +
                   (reverseBits8((uint8_t)prudata1[3] & 0xFF) << 29);
@@ -116,7 +177,9 @@ int main(void) {
                    current_up[position[2] % BUFFER_SIZE],
                    current_up[position[2] % BUFFER_SIZE],
                    current_up[position[2] % BUFFER_SIZE]);
+      pthread_mutex_lock(&serial_mutex);             
       write(fd, ajuste4setpoints, 22);
+      pthread_mutex_unlock(&serial_mutex);
       oldpos = position[2];
       gettimeofday(&tv2, NULL);
     }
@@ -134,12 +197,14 @@ int main(void) {
   return 0;
 }
 
-uint32_t reverseBits(uint32_t num) {
+uint32_t reverseBits(uint32_t num)
+{
   unsigned int count = sizeof(num) * 8 - 1;
   uint32_t reverse_num = num;
 
   num >>= 1;
-  while (num) {
+  while (num)
+  {
     reverse_num <<= 1;
     reverse_num |= num & 1;
     num >>= 1;
@@ -149,12 +214,14 @@ uint32_t reverseBits(uint32_t num) {
   return reverse_num;
 }
 
-uint8_t reverseBits8(uint8_t num) {
+uint8_t reverseBits8(uint8_t num)
+{
   unsigned int count = sizeof(num) * 8 - 1;
   uint8_t reverse_num = num;
 
   num >>= 1;
-  while (num) {
+  while (num)
+  {
     reverse_num <<= 1;
     reverse_num |= num & 1;
     num >>= 1;
@@ -165,27 +232,34 @@ uint8_t reverseBits8(uint8_t num) {
 }
 
 void adjustVector(uint8_t *data_vector, float current1, float current2,
-                  float current3, float current4) {
+                  float current3, float current4)
+{
   int i;
   // CURRENT 1
-  for (i = 0; i < 4; i++) {
+  for (i = 0; i < 4; i++)
+  {undefined reference to `mq_open'
+
     data_vector[5 + i] = (uint32_t)(*(uint32_t *)&current1) >> (i * 8);
   }
   // CURRENT 2
-  for (i = 0; i < 4; i++) {
+  for (i = 0; i < 4; i++)
+  {
     data_vector[9 + i] = (uint32_t)(*(uint32_t *)&current1) >> (i * 8);
   }
   // CURRENT 3
-  for (i = 0; i < 4; i++) {
+  for (i = 0; i < 4; i++)
+  {
     data_vector[13 + i] = (uint32_t)(*(uint32_t *)&current1) >> (i * 8);
   }
   // CURRENT 4FeedForward
-  for (i = 0; i < 4; i++) {
+  for (i = 0; i < 4; i++)
+  {
     data_vector[17 + i] = (uint32_t)(*(uint32_t *)&current1) >> (i * 8);
   }
   // CHECKSUM
   data_vector[21] = 0;
-  for (i = 0; i < 21; i++) {
+  for (i = 0; i < 21; i++)
+  {
     data_vector[21] -= data_vector[i];
   }
 }
